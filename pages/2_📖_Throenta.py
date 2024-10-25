3c201b451018315c35fb5d1557f1e8ad1ea5e907
Hurin12 <92066570+Hurin12@users.noreply.github.com>
2024-10-25 13:27:03 +0000

    first version created

---
+++ pages/2_📖_Throenta.py
@@ -0,0 +1,25 @@
+import streamlit as st
+import pandas as pd
+
+st.write("Welcome to page text")
+df = pd.read_excel("Diccionario dr.xlsm", sheet_name="Texts")
+df = df.dropna(how="all", axis=1).dropna(how="all", axis=0).reset_index(drop=True)
+df.columns=["spa", "dra", "spa_lit"]
+# st.write(df.head())
+show_literal = st.toggle("Show literal")
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    st.header("Espanol")
+    st.write("\n\n".join(df["spa"].dropna().values))
+
+with col2:
+    st.header("Draurir")
+    st.write("\n\n".join(df["dra"].dropna().values))
+
+with col3:
+    if show_literal:
+        st.header("Literal")
+        st.write("\n\n".join(df["spa_lit"].dropna().values))
+    else:
+        st.tabs(["Notes", "Vocab", "Media"])
