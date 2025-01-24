8bc74914627fe6d1dd9e43ca15841cef874f1026
t1emp0 <19372035+t1emp0@users.noreply.github.com>
2025-01-24 01:49:08 +0100

    Teonico: Now all labels are searchable

--- pages/1_📜_Teonico.py
+++ pages/1_📜_Teonico.py
@@ -27,36 +27,55 @@ def parse_definitions_examples(definition_str, example_str):
     return definitions, examples
 
 
+def generate_search_key(teonico: str, plural: str, verbs: str) -> str:
+    if not plural and not verbs:
+        return teonico
+    return f"{teonico} | {plural}{verbs}"
+
+
 with open("styles.css") as f:
     st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
 
 file_path = "data/teonicodicc.xlsx"
 
-st.markdown("<h1><a href='/' target='_self' class='invisible-link'>Diccionario Teónico</a></h1>", unsafe_allow_html=True)
+st.markdown(
+    "<h1><a href='/' target='_self' class='invisible-link'>Diccionario Teónico</a></h1>",
+    unsafe_allow_html=True,
+)
 
 df = pd.read_excel(file_path).dropna(how="all", axis=1)
 if df is None:
     st.error("Teonico excel file not loaded correctly")
     st.stop()
 
+verb_cols = [f"v{i}{j}" for i in range(1, 5) for j in range(1, 4)]
+df_extra = df[verb_cols].values.tolist()
+df["verbs_extra"] = [", ".join(v) if not pd.isna(v[0]) else None for v in df_extra]
+df["search_key"] = df.fillna("").apply(
+    lambda row: generate_search_key(row["teonico"], row["plural"], row["verbs_extra"]),
+    axis=1,
+)
 
 word = st.selectbox(
     labels.teo_dropdown_label,
-    sorted(df["teonico"]),
+    sorted(df["search_key"]),
     index=None,
     placeholder=labels.dropdown_placeholder,
+    format_func=normalize_string,
 )
-
 
 if word:
     word_row = (
-        df[df["teonico"].astype(str).apply(normalize_string) == normalize_string(word)]
+        df[
+            df["search_key"].astype(str).apply(normalize_string)
+            == normalize_string(word)
+        ]
         .iloc[0]
         .replace({np.nan: None})
     )
 
-    word_header = f"<h2 style='color:{constants.color_word_complex};'> {word} </h2>"
+    word_header = f"<h2 style='color:{constants.color_word_complex};'> {word_row["teonico"]} </h2>"
     st.markdown(word_header, unsafe_allow_html=True)
 
     if word_row["etimologia"]:
@@ -72,7 +91,6 @@ if word:
         st.write(plural)
 
     if word_row["cat1"] in constants.categories_verb:
-        verb_cols = [f"v{i}{j}" for i in range(1, 5) for j in range(1, 4)]
         current_verb = word_row[verb_cols]
         verb_table = current_verb.fillna("-").values.reshape(4, 3)
         st.table(verb_table)
